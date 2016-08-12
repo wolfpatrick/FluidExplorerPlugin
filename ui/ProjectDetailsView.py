03167d6335c73d3b1319f9d5c8ccc7e20afe01d8
wolfpatrick <patrickwolf1986@gmail.com>
2016-08-12 15:21:15 +0200

    Preview completed

--- ui/ProjectDetailsView.py
+++ ui/ProjectDetailsView.py
@@ -36,7 +36,8 @@ from ProjectDetailsViewUI import Ui_ProjectDetailsView
 
 from FluidExplorerPlugin.ui.Utils.DefaultUIValues import DefaultUIParameters
 from FluidExplorerPlugin.ui.Utils.ProjectDetailsViewUtils import ProjectSubSettings
-from FluidExplorerPlugin.ui.Utils.ProjectDetailsViewUtils import XMLReader
+from FluidExplorerPlugin.ui.Utils.ProjectDetailsViewUtils import ProjectDetailsViewUtils
+import os
 
 from maya import OpenMayaUI as omui
 from shiboken import wrapInstance
@@ -50,21 +51,31 @@ class windowTest(baseClass, widgetForm):
 """
 class ProjectDetailsView(QtGui.QDialog):
 
-    def __init__(self, args):
+    PERSPECTIVE_CAMERA_AVAILABLE = '1'
+    PERSPECTIVE_CAMERA_NOT_AVAILABLE = '0'
+
+    def __init__(self, args, pathToXMLFile):
         QtGui.QDialog.__init__(self, args)
 
-        # Stores configuration attributes
+        # Stores th path to the XML file
+        self.pathToXMLFile = pathToXMLFile
+
+        # Members
         self.projectSettings = None
+        self.hashMapToXMLProjectFile = {}
+        self.hashMapToGIF = {}
+        self.currentAnimationLoaded = None
 
         # Set up the user interface from the ui file
         self.ui = Ui_ProjectDetailsView()
         self.ui.setupUi(self)
-        #self.setWindowFlags(QtCore.Qt.Tool | QtCore.Qt.WindowStaysOnTopHint)
-        self.setWindowState(QtCore.Qt.WindowMinimizeButtonHint)
 
         # Move window to the 'modelPanel1' position
         [xPos, yPos] = self.moveWindowToPanel()
         self.move(xPos, yPos)
+
+        # QMovie
+        self.movie = QtGui.QMovie(self)
 
         # Create connections
         self.createConnections()
@@ -75,27 +86,41 @@ class ProjectDetailsView(QtGui.QDialog):
         self.initializeComponentss()
         self.setWindowTitle('Fluid Explorer - Simulation Details View')
 
-        # Set values from project configuration file
-        self.projectSettings = self.readProjectProperties('E:/TMP/ANNAANNA/ANNAANNA.fxp')
-        self.setValuesFromConfigurationFile(self.projectSettings)
-        print self.projectSettings.projectName
-
-        #####################################################
-        self.ui.comboBox_simulations.addItem('a')
-        self.ui.comboBox_simulations.addItem('a')
-        self.ui.comboBox_simulations.addItem('a')
-
+        # Window flags - top and buttons
         self.setWindowFlags(self.windowFlags() |
             QtCore.Qt.WindowMinimizeButtonHint |
             QtCore.Qt.WindowStaysOnTopHint)
 
+        self.show()
 
+        # Set values from project configuration file
+        statusCode = self.projectSettings = self.readProjectProperties(self.pathToXMLFile)
+        if statusCode:
+            canSetAllFields = self.setValuesFromConfigurationFile(self.projectSettings)
+            if not canSetAllFields:
+                self.showMessageBox('Could not read all project attributes from the\nconfiguration file!', 'warning')
+        else:
+            self.setAllFieldsEnabled()
 
+        # Initialize the combo box and the preview functionality
+        if self.projectSettings:
+            self.initComboBoxSimulations(self.projectSettings)
+            self.initPreview(self.projectSettings)
+
+    def setPathToProjectFile(self, path):
+        self.pathToXMLFile = path
+
+    def setAllFieldsEnabled(self):
+        self.ui.pushButton_applyCache.setEnabled(False)
+        self.ui.pushButton_exploreSimulations.setEnabled(False)
+        self.ui.checkBox_showPreview.setEnabled(False)
+        self.ui.comboBox_simulations.setEnabled(False)
+        self.update()
 
     def moveWindowToPanel(self):
         try:
             panelPtr = omui.MQtUtil.findControl('modelPanel1')
-            print panelPtr
+
             if not panelPtr:
                 xPos = 0
                 yPos = 0
@@ -119,12 +144,12 @@ class ProjectDetailsView(QtGui.QDialog):
         self.setMaximumWidth(340)
 
     def setWindowHeightWithPreview(self):
-        self.setMinimumHeight(622)
-        self.setMaximumHeight(622)
+        self.setMinimumHeight(620-25)
+        self.setMaximumHeight(620-25)
 
     def setWindowHeightWithoutPreview(self):
-        self.setMinimumHeight(360-1)
-        self.setMaximumHeight(360-1)
+        self.setMinimumHeight(360-0)
+        self.setMaximumHeight(360-0)
 
     def initializeComponentss(self):
         icon_help = QtGui.QIcon(QtGui.QPixmap(':/help_icon_orange.png'))
@@ -138,14 +163,75 @@ class ProjectDetailsView(QtGui.QDialog):
         self.setLineEditEnabledAndReadOnly(self.ui.lineEdit_startTime)
         self.setLineEditEnabledAndReadOnly(self.ui.lineEdit_endTime)
 
+        self.ui.label_moviePreview.setAlignment(QtCore.Qt.AlignCenter)
+        self.ui.label_moviePreview.setStyleSheet("background-color: black;")
+
+        self.scaleMovieLabel()
+        self.ui.label_moviePreview.setMaximumHeight(170)
+        self.ui.label_moviePreview.setMinimumHeight(170)
+
+    def scaleMovieLabel(self):
+        # Is supposed to be: 960x540
+        # Width = 300
+        size = self.ui.label_moviePreview.size()
+        newSize = QtCore.QSize(300, 169)
+        self.movie.setScaledSize(newSize)
+
     def setValuesFromConfigurationFile(self, projectSettings):
-         if projectSettings:
+        canReadAllAttributes = True
+
+        print("Read projects attributes for:\n")
+        for attr, value in projectSettings.__dict__.iteritems():
+            print attr, ": ", value
+            if value == None:
+                canReadAllAttributes = False
+                print("Warning: Cannot read attribute", attr)
+
+        if projectSettings:
             self.ui.lineEdit_projectName.setText(projectSettings.projectName)
             self.ui.lineEdit_projectPath.setText(projectSettings.projectPath)
             self.ui.lineEdit_fluidContainer.setText(projectSettings.fluidContainerName)
             self.ui.lineEdit_startTime.setText(projectSettings.animationStartTime)
             self.ui.lineEdit_endTime.setText(projectSettings.animationEndTime)
 
+        return canReadAllAttributes
+
+    def initComboBoxSimulations(self, projectSettings):
+
+        # Stores animation index and path
+        haspMap = {}
+
+        # First item
+        self.ui.comboBox_simulations.addItem('Select Simulation ...')
+
+        try:
+            num = int(projectSettings.numberOfSimulations)
+        except:
+            num = 0
+
+        if num > 0:
+            for i in range(num):
+
+                # Firste entry
+                tmpNameForElement = 'Simulation ' + str(i+1)
+
+                # Folders
+                tmp = projectSettings.projectPath + "/" + str(i) + "/"
+                if os.path.exists(tmp):
+                    pathToXMLProjectFileList = ProjectDetailsViewUtils.getPathToXMLFile(tmp)
+
+                    if len(pathToXMLProjectFileList) == 1:
+                        if os.path.exists(pathToXMLProjectFileList[0]):
+                            haspMap[i] = pathToXMLProjectFileList[0]
+                            self.ui.comboBox_simulations.addItem(tmpNameForElement)
+
+            self.hashMapToXMLProjectFile = haspMap
+
+    def initPreview(self, projectSettings):
+        if projectSettings.cam_persp == '1' or projectSettings.cam_persp == '1':
+            self.hashMapToGIF = ProjectDetailsViewUtils.getGIFHashMap(projectSettings)
+        else:
+            self.ui.checkBox_showPreview.setEnabled(False)
 
     def createConnections(self):
         self.connect(self.ui.pushButton_applyCache, QtCore.SIGNAL("clicked()"), self.applyCacheClicked)
@@ -153,11 +239,59 @@ class ProjectDetailsView(QtGui.QDialog):
         self.connect(self.ui.checkBox_showPreview, QtCore.SIGNAL("stateChanged(int)"), self.checkBoxPreviewValueChanged)
         self.connect(self.ui.comboBox_simulations, QtCore.SIGNAL("currentIndexChanged(QString)"), self.comboBoxSimulationsIndexChanged)
         self.connect(self.ui.pushButton_help, QtCore.SIGNAL("clicked()"), self.helpButtonClicked)
+        self.connect(self.movie, QtCore.SIGNAL("frameChanged(int)"), self.frameChangedHandler)
+
+    def playAnimation(self, simulationIndex):
+
+        if self.hashMapToGIF == None:
+            return
+
+        # Index for the hash map
+        hashIndex = simulationIndex - 1
+
+        if simulationIndex == 0:
+            self.stopPlayingAnimation()
+            return
+
+        fileName = self.hashMapToGIF[hashIndex]
+
+        if not os.path.exists(fileName):
+            self.ui.label_moviePreview.setText("<b>[ Cannot find animation ... ]</b>")
+            self.stopPlayingAnimation()
+            return
+        else:
+            self.ui.label_moviePreview.setText("")
+            self.ui.label_moviePreview.setMovie(self.movie)
+
+        # Play animation
+        currentState = self.movie.state()
+
+        if currentState == QtGui.QMovie.Running:
+            self.movie.stop()
+            self.movie.setFileName(fileName)
+            self.movie.start()
+
+        elif currentState == QtGui.QMovie.NotRunning:
+            self.movie.setFileName(fileName)
+            self.movie.start()
+
+    def stopPlayingAnimation(self):
+        self.movie.stop()
+        self.ui.label_moviePreview.setText("<b>[ No Simulation selected ... ]</b>")
 
     # - Event handlers -
     @QtCore.Slot()
     def applyCacheClicked(self):
-        print 'applyCacheClicked clicked'
+        currentIndex = self.ui.comboBox_simulations.currentIndex()
+        if currentIndex >= 1:
+            pathToXMLFile = self.hashMapToXMLProjectFile[currentIndex-1]
+            if pathToXMLFile == self.currentAnimationLoaded:
+                # Do not load the cache which is already loaded! -> pass
+                pass
+            else:
+                print('Load cache file', pathToXMLFile)
+                self.currentAnimationLoaded = pathToXMLFile
+
 
     @QtCore.Slot()
     def exploreSimulationsClicked(self):
@@ -165,25 +299,34 @@ class ProjectDetailsView(QtGui.QDialog):
 
     @QtCore.Slot()
     def checkBoxPreviewValueChanged(self, state):
-        print 'checkBoxPreviewValueChanged'
-        print self.ui.checkBox_showPreview.checkState()
         # State starts with 0
-
         if self.ui.checkBox_showPreview.checkState() == QtCore.Qt.Checked:
-            print "CHECKED"
+            # active
             self.setWindowHeightWithPreview()
+            self.playAnimation(self.ui.comboBox_simulations.currentIndex())
         elif self.ui.checkBox_showPreview.checkState() == QtCore.Qt.Unchecked:
-            print "NOT CHECKED"
+            # not active
             self.setWindowHeightWithoutPreview()
+            self.movie.stop()
 
     @QtCore.Slot()
     def comboBoxSimulationsIndexChanged(self, index):
-        print 'ecomboBoxSimulationsIndexChanged'
-        print self.ui.comboBox_simulations.currentIndex()
+        if self.ui.checkBox_showPreview.checkState() == QtCore.Qt.Checked:
+            currentIndex = self.ui.comboBox_simulations.currentIndex()
+            #hashMapIndex = currentIndex-1
+
+            # Play animation
+            self.playAnimation(currentIndex)
 
     @QtCore.Slot()
     def helpButtonClicked(self):
         print 'help'
+
+    @QtCore.Slot()
+    def frameChangedHandler(self, frameNumber):
+        pass
+        #print frameNumber
+
     # - Event handlers end -
 
     # - Help functions -
@@ -212,14 +355,14 @@ class ProjectDetailsView(QtGui.QDialog):
         return styleEnabled
 
     def readProjectProperties(self, pathToXMLFile):
-        xmReader = XMLReader()
+        xmReader = ProjectDetailsViewUtils()
 
         projectSettings = None
         try:
             projectSettings = xmReader.getProjectSubSettings(pathToXMLFile)
         except Exception as e:
             errorText = "An error occured while loading the project configuration file!\nDetails: " + str(e.message)
-            self.showMessageBox(errorText, 'warning')
+            self.showMessageBox(errorText, 'critical')
             return projectSettings
 
         print projectSettings.projectName
@@ -238,4 +381,4 @@ class ProjectDetailsView(QtGui.QDialog):
 
         msgBox.setStyleSheet(DefaultUIParameters.buttonStyleBold)
         msgBox.exec_()
-        # - Help functions end -
+    # - Help functions end -
--- ui/Utils/ProjectDetailsViewUtils.py
+++ ui/Utils/ProjectDetailsViewUtils.py
@@ -14,7 +14,7 @@ class ProjectSubSettings():
 import os
 import xml.etree.cElementTree as ET
 
-class XMLReader():
+class ProjectDetailsViewUtils():
 
     def __init__(self):
         pass
@@ -44,14 +44,61 @@ class XMLReader():
     def getProjectSubSettings(self, xml_file):
         projectSettings = ProjectSubSettings()
         try:
-            projectSettings.projectName = XMLReader.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectName')
-            projectSettings.projectPath = XMLReader.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectPath')
-            projectSettings.fluidContainerName = XMLReader.readAttributeFromXmlConfigurationsFile(xml_file, 'FluidBoxName')
-            projectSettings.animationStartTime = XMLReader.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectName')
-            projectSettings.animationEndTime = XMLReader.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectName1')
+            projectSettings.projectName = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectName')
+            projectSettings.projectPath = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'ProjectPath')
+            projectSettings.fluidContainerName = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'FluidBoxName')
+            projectSettings.numberOfSimulations = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'Samples')
+            projectSettings.animationStartTime = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'TODO')
+            projectSettings.animationEndTime = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'TODO')
+            projectSettings.cam_persp = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'PerspectiveCamera')
+            projectSettings.cam_vc = ProjectDetailsViewUtils.readAttributeFromXmlConfigurationsFile(xml_file, 'ViewCubeCamera')
 
         except Exception as e:
             print "Error: Cannot read project attributes! Details: " + str(e.message)
             raise Exception(e.message)
 
         return projectSettings
+
+    @staticmethod
+    def getPathToXMLFile(path):
+        list_dir = []
+        files_xml = []
+        list_dir = os.listdir(path)
+        count = 0
+        for file in list_dir:
+            if file.endswith('xml'):
+                count += 1
+                tmp = path + file
+                pathNew = os.path.abspath(tmp)
+                files_xml.append(pathNew.replace('\\','/'))
+
+        return files_xml
+
+    @staticmethod
+    def getGIFHashMap(projectSettings):
+        hashMapToGIF = {}
+
+        try:
+            num = int(projectSettings.numberOfSimulations)
+        except:
+            num = 0
+
+        if num > 0:
+            for i in range(num):
+                if projectSettings.cam_persp == '1':
+                    tmp = '{0}/{1}/{2}/{3}/{4}'.format(projectSettings.projectPath, i, 'images', 'perspective', 'animation.gif')
+                    print tmp
+                    path = os.path.abspath(tmp)
+                    path = path.replace('\\', '/')
+                    if os.path.exists(path):
+                        print path
+                        hashMapToGIF[i] = path
+
+                elif projectSettings.cam_vc == '1':
+                    tmp = '{0}/{1}/{2}/{3}/{4}/{5}'.format(projectSettings.projectPath, i, 'images', 'viewcube', 'front', 'animation.gif')
+                    path = os.path.abspath(tmp)
+                    path = path.replace('\\', '/')
+                    if os.path.exists(path):
+                        hashMapToGIF[i] = path
+
+        return hashMapToGIF
